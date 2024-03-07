dc0174aee62f6b3a0dbd1121d7ed09c7957fa1c3
Abhijeet Waghmare <149177534+abhijeetw035@users.noreply.github.com>
2024-03-07 14:27:10 +0530

    Time: 3 ms (64.82%) | Memory: 11.3 MB (34.14%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -19,7 +19,7 @@ public:
         while(low<=high){
             int mid = (high + low)/2;
             if(nums[mid]>nums[mid+1] and nums[mid]>nums[mid-1]) return mid;
-            else if(nums[mid]>nums[mid-1] and nums[mid]<nums[mid+1]) low = mid + 1;
+            else if(nums[mid]>nums[mid-1]) low = mid + 1;
             else high = mid - 1;
         }
         return 0;
